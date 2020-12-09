21e31b3b06d1235659178b9c9eae8d1cdcdd2b38
Carlos Linares <contact@carloslinares.eu>
2020-12-09 05:18:49 +0100

    New login method

--- run.ps1
+++
@@ -1,4 +0,0 @@
-Write-Host "Congratulations! Your first script executed successfully"
-$env:GOOGLE_APPLICATION_CREDENTIALS="gcloud\Text-2-Speech-ACFrance-d1b16b239b54.json"
-.\venv\Scripts\activate
-.\text2speech\core.py
