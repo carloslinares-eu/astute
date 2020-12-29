0d8ad88f475bad2e2f212dc2e36d841a52b2a56f
Carlos Linares <contact@carloslinares.eu>
2020-12-29 19:42:04 +0100

    Adding new GUI elements

--- as2t/as2t.py
+++ as2t/as2t.py
@@ -1,105 +1,55 @@
 import tkinter
 import tkinter.messagebox
 import tkinter.scrolledtext
+import tkinter.ttk
 import PIL.ImageTk
 import PIL.Image
 
 main_window = tkinter.Tk()
 main_window.title("AS2T - ATEXIS Text-to-Speech App")
-main_window.geometry("640x480")
+main_window.geometry("480x480")
 main_window.iconbitmap("as2t/as2t.ico")
+text_format_01 = ("Segoe UI", 10)
+main_width = 60
+
+language_codes = ["en-GB", "en-US", "es-ES"]
+genders = ["Male", "Female"]
+voice_types = ["Regular", "Wavenet"]
+available_voices = ["B", "A"]
 
 
-def dummy_command():
+def check_voices():
     pass
 
 
-def show():
-    show_hide_frame.grid(row=1, column=0, columnspan=2, padx=20, pady=20)
+input_text_label = tkinter.Label(main_window, text="Input text", anchor="w")
+input_text_box = tkinter.scrolledtext.ScrolledText(main_window, font=text_format_01, width=main_width, height=6)
+
+language_code_label = tkinter.Label(main_window, text="Language Code", anchor="w")
+language_code_dropdown = tkinter.ttk.Combobox(main_window, value=language_codes)
+
+gender_label = tkinter.Label(main_window, text="Gender")
+gender_dropdown = tkinter.ttk.Combobox(main_window, value=genders)
+
+voice_type_label = tkinter.Label(main_window, text="Voice Type")
+voice_type_dropdown = tkinter.ttk.Combobox(main_window, value=voice_types)
+
+voice_selection_label = tkinter.Label(main_window, text="Voice Name")
+voice_check_button = tkinter.ttk.Button(main_window, text="Check available voices", command=check_voices)
+voice_selection_dropdown = tkinter.ttk.Combobox(main_window, value=available_voices)
 
 
-def hide():
-    show_hide_frame.grid_forget()
+input_text_label.grid(padx=20, pady=5, row=0, column=0, sticky="w")
+input_text_box.grid(padx=20, pady=5, row=1, column=0, columnspan=3, rowspan=6)
 
+language_code_label.grid(padx=20, pady=5, row=8, column=0, sticky="w")
+language_code_dropdown.grid(padx=20, pady=5, row=8, column=2, sticky="e")
 
-def radio():
-    radio_label = tkinter.Label(main_window, text=regular_variable.get())
-    radio_label.grid(row=4, column=2)
+gender_label.grid(padx=20, pady=5, row=9, column=0, sticky="w")
+gender_dropdown.grid(padx=20, pady=5, row=9, column=2, sticky="e")
 
-
-def popup():
-    tkinter.messagebox.askquestion(title="Warning message", message="Bad!")
-
-
-# atexis_logo = PIL.ImageTk.PhotoImage(PIL.Image.open("as2t/data/images/atexis.png"))
-# atexis_logo_label = tkinter.Label(image=atexis_logo)
-# atexis_logo_label.grid(row=0, column=0)
-
-# text_entry = tkinter.Entry(main_window, width=300, font=("Segoe UI", 9))
-# text_entry.grid(row=1, column=1)
-
-# Define Menu
-main_menu = tkinter.Menu(main_window)
-main_window.config(menu=main_menu)
-
-# Create Menu Items
-file_menu = tkinter.Menu(main_menu, tearoff=0)
-main_menu.add_cascade(label="File", menu=file_menu)
-file_menu.add_command(label="New", command=dummy_command)
-file_menu.add_separator()
-file_menu.add_command(label="Exit", command=main_window.quit)
-
-# Create another submenu Edit
-edit_menu = tkinter.Menu(main_menu, tearoff=0)
-main_menu.add_cascade(label="Edit", menu=edit_menu)
-edit_menu.add_command(label="Cut", command=dummy_command)
-edit_menu.add_command(label="Copy", command=dummy_command)
-edit_menu.add_command(label="Paste", command=dummy_command)
-
-show_button = tkinter.Button(main_window, text="Show", command=show)
-hide_button = tkinter.Button(main_window, text="Hide", command=hide)
-
-show_button.grid(row=0, column=0)
-hide_button.grid(row=0, column=1)
-
-show_hide_frame = tkinter.Frame(main_window, width=200, heigh=200, bd=5, bg="blue", relief="sunken")
-show_hide_frame.grid(row=1, column=0, columnspan=2, padx=20, pady=20)
-
-show_hide_frame_label = tkinter.Label(show_hide_frame, text="Hello World", font=("Helvetica", 20))
-show_hide_frame_label.pack(padx=20, pady=20)
-
-status = tkinter.Label(main_window, text="Waiting", bd=2, relief="sunken", width=60)
-status.grid(row=2, column=0)
-
-regular_variable = tkinter.IntVar()
-
-radio_button_01 = tkinter.Radiobutton(main_window, text="One", variable=regular_variable, value=1)
-radio_button_02 = tkinter.Radiobutton(main_window, text="Two", variable=regular_variable, value=2)
-
-radio_button_01.grid(row=2, column=2)
-radio_button_02.grid(row=2, column=3)
-
-radio_control = tkinter.Button(main_window, text="Click Me", command=radio)
-radio_control.grid(row=3, column=2)
-
-# Create checkboxes
-var = tkinter.BooleanVar()
-checkbutton = tkinter.Checkbutton(main_window, text="Pepperoni", variable=var, onvalue=True, offvalue=False)
-checkbutton.grid(row=3, column=1)
-
-pop_button = tkinter.Button(main_window, text="Click to Popup", command=popup)
-pop_button.grid(row=4, column=1)
-
-text_style_01 = ("Segoe UI", 10)
-
-text_entry_label = tkinter.Label(main_window, text="Input text", font=text_style_01, anchor="sw")
-text_entry_label.grid(row=0, column=0, pady=20, padx=20, sticky="W")
-
-# atexis_logo = PIL.ImageTk.PhotoImage(PIL.Image.open("as2t/data/images/atexis.png"))
-# atexis_logo_label = tkinter.Label(image=atexis_logo)
-# atexis_logo_label.grid(row=0, column=0)
-
-text_entry = tkinter.scrolledtext.ScrolledText(main_window, font=text_style_01)
-text_entry.grid(row=1, column=0, columnspan=2, padx=20, sticky="W")
+voice_selection_label.grid(padx=20, pady=5, row=10, column=0, sticky="w")
+voice_check_button.grid(pady=5, row=10, column=1, sticky="e")
+voice_selection_dropdown.grid(padx=20, pady=5, row=10, column=2, sticky="e")
 
 main_window.mainloop()
--- as2t/english_estandard.py
+++
@@ -1,7 +0,0 @@
-import as2t.login.local
-
-credentials_file_path = "./as2t/login/gcloud_credentials.json"
-
-credentials, scoped_credentials = as2t.login.local.login_with_json(credentials_file_path)
-
-
